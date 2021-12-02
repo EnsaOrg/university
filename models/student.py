ca160863599c129204428d77d22253bc632415e2
ambou-97 <ambroise.tine@gmail.com>
2021-12-02 15:53:42 +0100

    bare d'états dans les formulaires

--- models/student.py
+++ models/student.py
@@ -15,6 +15,8 @@ class UniversityStudent(models.Model):
      email = fields.Char()
      phone = fields.Char()
 
+     state = fields.Selection([('L1','Level 1'),('L2','Level 2'),('L3','Level 3'),('finished','Finished')])
+
      department_id = fields.Many2one(comodel_name='university.department')
      classroom_id = fields.Many2one(comodel_name='university.classroom')
 
@@ -34,3 +36,16 @@ class UniversityStudent(models.Model):
      def check_dates(self):
           if self.birthday > self.registration_date:
                raise ValueError('The birthday must be inferior than the registration date')
+
+
+     @api.multi
+     def nex_level(self):
+          if self.state == '':
+               return self.write({'state':'L2'})
+          elif self.state == 'L2':
+               return self.write({'state':'L3'})
+          elif self.state == 'L3':
+               return self.write({'state': 'finished'})
+          elif self.state == 'finished':
+               return {'warning':{'title':'Finished',
+                                  'message':'This student has already finished his courses!'}}
